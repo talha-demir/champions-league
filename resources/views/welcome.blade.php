559c19afbe60f9cee71d944c8eb8181323b1bfcc
Talha Demir <talha.demir@kolaybi.com>
2022-07-09 12:38:40 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
+<body>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+@livewireScripts
+</body>
+</html>
