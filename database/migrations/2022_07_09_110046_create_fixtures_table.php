46bd4471b873efcc9633a98277e367057dbf49e6
Talha Demir <talha_td@hotmail.com>
2022-07-09 14:11:30 +0300

    Create teams table and fixtures table

---
+++ database/migrations/2022_07_09_110046_create_fixtures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fixtures', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('home_team_id')->constrained('teams');
+            $table->foreignId('away_team_id')->constrained('teams');
+            $table->integer('week');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fixtures');
+    }
+};
