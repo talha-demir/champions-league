7be79b6f1f6370e18d332ab32241a6e4cc966951
Talha Demir <talha_td@hotmail.com>
2022-07-14 02:06:10 +0300

    Change css url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link href={{secure_asset('resources/css/app.css')}} rel="stylesheet"
+    <link href={{secure_asset('build/assets/app.493c36fd.css')}} rel="stylesheet"
     @livewireStyles
 </head>
 <body>
